d6b339e5dfdba18fbc28bbc18fc3b8aa53157314
gajendra singh <gajendra09822@gmail.com>
2022-08-01 17:16:49 +0530

    job application added

--- newapp/urls.py
+++ newapp/urls.py
@@ -1,5 +1,16 @@
 from django.urls import path, include
+from rest_framework import routers
+from .views import *
+
+router = routers.DefaultRouter()
+
+
+router.register(r'jobs', JobApplication, basename='Job')
+
 
 urlpatterns = [
-   # path('auth/', include('rest_auth.urls')),
+   path('', include(router.urls)),
+   path('api-auth/', include('rest_framework.urls')),
+
+
 ]
